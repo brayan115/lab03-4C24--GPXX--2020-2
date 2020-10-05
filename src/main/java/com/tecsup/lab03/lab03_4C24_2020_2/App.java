a993c5864e67e93208ddedfabb2d0e6e447e99bd
sschambaher <sebastianschambahermail.com>
2020-10-05 17:24:49 -0500

    Commit by schambaher

--- src/main/java/com/tecsup/lab03/lab03_4C24_2020_2/App.java
+++ src/main/java/com/tecsup/lab03/lab03_4C24_2020_2/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "HOLA CLASE !!" );
+        System.out.println( "Schambaher Gallegos Hizo un cambio !!!" );
         System.out.println("GRUPO 6A");
         System.out.println("INTEGRANTES:");
         System.out.println("Cardenas Daniel");
